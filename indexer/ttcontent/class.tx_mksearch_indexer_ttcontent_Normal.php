50bb253d9f147d2fd3c3daec84cc77b028e1915a
Michael Wagner <michael.wagner@das-medienkombinat.de>
2012-04-10 11:38:33 +0000

    indexer has to implement interface

--- indexer/ttcontent/class.tx_mksearch_indexer_ttcontent_Normal.php
+++ indexer/ttcontent/class.tx_mksearch_indexer_ttcontent_Normal.php
@@ -38,25 +38,10 @@ tx_rnbase::load('tx_mksearch_util_Misc');
 /**
  * Indexer service for core.tt_content called by the "mksearch" extension.
  * takes care of normal tt_content without templavoila support.
- * 
+ *
  * @author Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
  */
 class tx_mksearch_indexer_ttcontent_Normal extends tx_mksearch_indexer_Base {
-
-	/**
-	 * Return content type identification.
-	 * This identification is part of the indexed data
-	 * and is used on later searches to identify the search results.
-	 * You're completely free in the range of values, but take care
-	 * as you at the same time are responsible for
-	 * uniqueness (i.e. no overlapping with other content types) and
-	 * consistency (i.e. recognition) on indexing and searching data.
-	 *
-	 * @return array([extension key], [key of content type])
-	 */
-	public static function getContentType() {
-		return array('core', 'tt_content');
-	}
 
 	/**
 	 * @see tx_mksearch_indexer_Base::indexData()
@@ -284,101 +269,6 @@ class tx_mksearch_indexer_ttcontent_Normal extends tx_mksearch_indexer_Base {
 	}
 
 	/**
-	 * Return the default Typoscript configuration for this indexer.
-	 *
-	 * Note that this config is not used for actual indexing
-	 * but only serves as assistance when actually configuring an indexer!
-	 * Hence all possible configuration options should be set or
-	 * at least be mentioned to provide an easy-to-access inline documentation!
-	 *
-	 * @return string
-	 *
-	 */
-	public function getDefaultTSConfig() {
-		return <<<CONF
-# Fields which are set statically to the given value
-# Don't forget to add those fields to your Solr schema.xml
-# For example it can be used to define site areas this
-# contentType belongs to
-#
-# fixedFields{
-#	my_fixed_field_singlevalue = first
-#   my_fixed_field_multivalue{
-#      0 = first
-#      1 = second
-#   }
-# }
-
-addPageMetaData = 0
-addPageMetaData.separator = ,
-
-# Configuration for each cType:
-CType {
-	# Default configuration for unconfigured cTypes:
-	_default_ {
-		# Fields used for building the index:
-		indexedFields {
-			0 = bodytext
-			1 = imagecaption
-			2 = altText
-			3 = titleText
-		}
-	}
-	# cType "text":
-	text.indexedFields {
-		0 = bodytext
-	}
-}
-
-# cTypes of content elements to be excluded from indexing.
-# Obviously, the respective "indexedFields" option is ignored in this case.
-#includeCTypes = list,header,text,textpic,image,text,bullets,table,html
-ignoreCTypes = search,mailform,login,list,div
-
-# \$sys_language_uid of the desired language
-# lang = 1
-
-### delete from or abort indexing for the record if isIndexableRecord or no record?
- deleteIfNotIndexable = 0
-
-# White lists: Explicitely include items in indexing by various conditions.
-# Note that defining a white list deactivates implicite indexing of ALL pages,
-# i.e. only white-listed pages are defined yet!
-# May also be combined with option "exclude"
-include {
-	# Include several content elements pages in indexing:
-#	elements {
-#		# Include tt_content #17
-#		0 = 17
-#		# Include tt_content #26
-#		1 = 26
-#	}
-# Include several pages in indexing:
-#		# Include page #18 and #27
-#	pages = 18,27
-# Include complete page trees (i. e. pages with all their children) in indexing:
-#	pageTrees {
-#		# Include page tree with root page #19
-#		0 = 19
-#		# Include page  tree with root page #28
-#		1 = 28
-#	}
-}
-# Black lists: Exclude pages from indexing by various conditions.
-# May also be combined with option "include", while "exclude" option
-# takes precedence over "include" option.
-exclude {
-	# Exclude several pages from indexing. @see respective include option
-#	pages ...
- 	# Exclude complete page trees (i. e. pages with all their children) from indexing.
- 	# @see respective include option
-#	pageTrees ...
-}
-
-CONF;
-	}
-	
-	/**
 	* Sets the index doc to deleted if neccessary
 	* @param tx_rnbase_model_base $oModel
 	* @param tx_mksearch_interface_IndexerDocument $oIndexDoc
@@ -389,7 +279,7 @@ CONF;
 		//care of all possible hidden parent pages
 		return (!$this->checkPageRights($oModel) || parent::hasDocToBeDeleted($oModel,$oIndexDoc,$aOptions));
 	}
-	
+
 	/**
 	 * @see tx_mksearch_indexer_Base::isIndexableRecord()
 	 */
@@ -400,7 +290,7 @@ CONF;
 		) {
 			return true;
 		}
-	
+
 		return false;
 	}
 }
