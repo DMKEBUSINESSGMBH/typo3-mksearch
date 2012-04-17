e243c83237fa3fd53abe4082f99ceac23a724b46
Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
2012-04-17 17:54:31 +0000

    leiner bugfix

--- service/indexer/core/class.tx_mksearch_service_indexer_core_Config.php
+++ service/indexer/core/class.tx_mksearch_service_indexer_core_Config.php
@@ -74,7 +74,7 @@ class tx_mksearch_service_indexer_core_Config {
 	 * bug (http://forge.typo3.org/issues/36232) exists 
 	 */
 	public static function clearPageInstance() {
-		unset(self::$page);
+		self::$page = null;
 	}
 	
 	/**
