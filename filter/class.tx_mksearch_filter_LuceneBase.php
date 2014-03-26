e8478f4906cbba900be5f0eabf44a8ea0bc8c704
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-03-26 17:17:24 +0100

    #356-77: fehlender load

--- filter/class.tx_mksearch_filter_LuceneBase.php
+++ filter/class.tx_mksearch_filter_LuceneBase.php
@@ -25,6 +25,7 @@ require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
 
 tx_rnbase::load('tx_rnbase_util_SearchBase');
 tx_rnbase::load('tx_rnbase_util_ListBuilderInfo');
+tx_rnbase::load('tx_rnbase_filter_BaseFilter');
 
 /**
  * Dieser Filter verarbeitet Anfragen für Lucene
