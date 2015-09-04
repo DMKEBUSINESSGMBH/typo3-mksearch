02f9923dfd996707312e6075cde31ea7adbcfc73
René Nitzsche <nitzsche@dmk-ebusiness.de>
2015-09-04 14:41:54 +0200

    #7 TS parsing used from rn_base

--- util/class.tx_mksearch_util_Misc.php
+++ util/class.tx_mksearch_util_Misc.php
@@ -194,11 +194,8 @@ class tx_mksearch_util_Misc {
 	 * @param string $sTs
 	 */
 	public static function parseTsConfig($sTs) {
-		/* @var $TSparserObject t3lib_tsparser */
-		$TSparserObject = tx_rnbase::makeInstance('t3lib_TSparser');
-		$sTs = $TSparserObject->checkIncludeLines($sTs);
-		$TSparserObject->parse($sTs);
-		return $TSparserObject->setup;
+		tx_rnbase::load('tx_rnbase_util_TS');
+		return tx_rnbase_util_TS::parseTsConfig($sTs);
 	}
 
 	/**
