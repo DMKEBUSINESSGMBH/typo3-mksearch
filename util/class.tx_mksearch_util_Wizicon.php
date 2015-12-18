35f17e5c2173ac79b2cc4be55b9d70f3b0a5d844
Eric Hertwig <eric.hertwig@dmk-ebusiness.de>
2015-12-18 13:30:23 +0100

    [Bugfix] Show default labels for wizicon if label is not in languageFile - using tx_rnbase_util_Wizicon

--- util/class.tx_mksearch_util_Wizicon.php
+++ util/class.tx_mksearch_util_Wizicon.php
@@ -1,64 +1,64 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2009-2012 DMK E-Business GmbH
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2009-2012 DMK E-Business GmbH
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
- * Class that adds the wizard icon.
+ *
+ * tx_mksearch_util_Wizicon
+ *
+ * @package        TYPO3
+ * @subpackage     mkserach
+ * @author         Eric Hertwig <dev@dmk-ebusiness.de>
+ * @license        http://www.gnu.org/licenses/lgpl.html
+ *                 GNU Lesser General Public License, version 3 or later
  */
-class tx_mksearch_util_Wizicon {
+
+tx_rnbase::load( 'tx_rnbase_util_Extensions' );
+tx_rnbase::load( 'tx_rnbase_util_Wizicon' );
+
+class tx_mksearch_util_Wizicon extends tx_rnbase_util_Wizicon {
+
 	/**
-	 * Adds the plugin wizard icon
-	 *
-	 * @param array Input array with wizard items for plugins
-	 * @return array Modified input array, having the items for plugin added.
+	 * @return array
 	 */
-	function proc($wizardItems)	{
-		global $LANG;
-
-		$LL = $this->includeLocalLang();
-
-		$wizardItems['plugins_tx_mksearch'] = array(
-			'icon'=>t3lib_extMgm::extRelPath('mksearch').'/ext_icon.gif',
-			'title'=>$LANG->getLLL('plugin.mksearch.label',$LL),
-			'description'=>$LANG->getLLL('plugin.mksearch.description',$LL),
-			'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=tx_mksearch'
+	protected function getPluginData() {
+		return array(
+			'plugins_tx_mksearch' => array(
+				'icon'        => tx_rnbase_util_Extensions::extRelPath( 'mksearch' ) . 'ext_icon.gif',
+				'title'       => 'plugin.mksearch.label',
+				'description' => 'plugin.mksearch.description'
+			)
 		);
-
-		return $wizardItems;
 	}
-	function includeLocalLang()	{
-		$llFile = t3lib_extMgm::extPath('mksearch').'locallang_db.xml';
-		if (tx_rnbase_util_TYPO3::isTYPO46OrHigher()) {
-			$llXmlParser = tx_rnbase::makeInstance('t3lib_l10n_parser_Llxml');
-			$LOCAL_LANG =  $llXmlParser->getParsedData($llFile, $GLOBALS['LANG']->lang);
-		}
-		else {
-			$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
-		}
-		return $LOCAL_LANG;
+
+	/**
+	 * @return string
+	 */
+	protected function getLLFile() {
+		return tx_rnbase_util_Extensions::extPath( 'mksearch' ) . 'locallang_db.xml';
 	}
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mksearch/util/class.tx_mksearch_util_Wizicon.php'])	{
-	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mksearch/util/class.tx_mksearch_util_Wizicon.php']);
+if ( defined( 'TYPO3_MODE' ) && $GLOBALS['TYPO3_CONF_VARS'][ TYPO3_MODE ]['XCLASS']['ext/mksearch/util/class.tx_mksearch_util_Wizicon.php'] ) {
+	include_once( $GLOBALS['TYPO3_CONF_VARS'][ TYPO3_MODE ]['XCLASS']['ext/mksearch/util/class.tx_mksearch_util_Wizicon.php'] );
 }
