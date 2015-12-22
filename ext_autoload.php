3e36f181c3ede091a29852e7a0f2a63b33114927
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2015-12-22 08:15:47 +0100

    AC#235-3372 autoloading gefixed

--- ext_autoload.php
+++ ext_autoload.php
@@ -3,7 +3,11 @@
  * Register necessary class names with autoloader
  *
  */
+// wir können weder den Extensionmanager nutzen (auf Grund der verschiedenen TYPO3 Versionen)
+// noch tx_rnbase_util_Extensions (weil noch nicht geladen). Also nehmen wir den Pfad hart. Dürfte
+// aber kein Problem sein.
+$extensionPath = PATH_typo3conf . 'ext/mksearch/';
 return array(
-	'tx_mksearch_scheduler_indextask'					=> tx_rnbase_util_Extensions::extPath('mksearch', 'scheduler/class.tx_mksearch_scheduler_IndexTask.php'),
-	'tx_mksearch_scheduler_indextaskaddfieldprovider'	=> tx_rnbase_util_Extensions::extPath('mksearch', 'scheduler/class.tx_mksearch_scheduler_IndexTaskAddFieldProvider.php'),
+	'tx_mksearch_scheduler_indextask'					=> $extensionPath . 'scheduler/class.tx_mksearch_scheduler_IndexTask.php',
+	'tx_mksearch_scheduler_indextaskaddfieldprovider'	=> $extensionPath . 'scheduler/class.tx_mksearch_scheduler_IndexTaskAddFieldProvider.php',
 );
