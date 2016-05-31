249ad445f87e28deb679351a23dc3e6d6e075e95
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2016-05-31 10:01:42 +0200

    fixed tests
    #15

--- tests/filter/class.tx_mksearch_tests_filter_FacetBase_testcase.php
+++ tests/filter/class.tx_mksearch_tests_filter_FacetBase_testcase.php
@@ -156,6 +156,9 @@ class tx_mksearch_tests_filter_FacetBase_testcase
 		//force noch setzen
 		$config['searchsolr.']['filter.']['default.']['force'] = 1;
 
+		// brauchen wir hier nicht
+		unset($config['searchsolr.']['filter.']['default.']['options.']['fq.'][1464679148]);
+
 		return $config;
 	}
 
