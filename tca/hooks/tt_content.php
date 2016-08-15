e092641ad875342d8c6f3c0333221caa1a117f1f
Christian Hofmann <christian.hofmann@dmk-ebusiness.de>
2016-08-15 13:44:30 +0200

    AC#3-3613 Anpassung des veralteten Aufrufs von addTCAcolumns

--- tca/hooks/tt_content.php
+++ tca/hooks/tt_content.php
@@ -5,32 +5,35 @@ if (!defined ('TYPO3_MODE')) {
 
 // Extend table tt_news
 tx_rnbase::load('tx_mksearch_indexer_ttcontent_Normal');
-
-$tempTtContent = array(
-	'tx_mksearch_is_indexable' => array(
-		'exclude' => 1,
-		'label' => 'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable',
-		'config' => array (
-			'type'	=> 'select',
-			'renderType' => 'selectSingle',
-			'items' => array(
-    			array(
-    				'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable.use_indexer_config',
-    				tx_mksearch_indexer_ttcontent_Normal::USE_INDEXER_CONFIGURATION
-    			),
-    			array(
-    				'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable.yes',
-    				tx_mksearch_indexer_ttcontent_Normal::IS_INDEXABLE
-    			),
-    			array(
-    				'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable.no',
-    				tx_mksearch_indexer_ttcontent_Normal::IS_NOT_INDEXABLE
-    			),
-			),
-			'default' => tx_mksearch_indexer_ttcontent_Normal::USE_INDEXER_CONFIGURATION
-		)
+tx_rnbase::load('tx_rnbase_util_TYPO3');
+tx_rnbase_util_Extensions::addTCAcolumns(
+	'tt_content',
+	array(
+		'tx_mksearch_is_indexable' => array(
+			'exclude' => 1,
+			'label' => 'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable',
+			'config' => array (
+				'type'	=> 'select',
+				'renderType' => 'selectSingle',
+				'items' => array(
+					array(
+						'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable.use_indexer_config',
+						tx_mksearch_indexer_ttcontent_Normal::USE_INDEXER_CONFIGURATION
+					),
+					array(
+						'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable.yes',
+						tx_mksearch_indexer_ttcontent_Normal::IS_INDEXABLE
+					),
+					array(
+						'LLL:EXT:mksearch/locallang_db.xml:tt_content.tx_mksearch_is_indexable.no',
+						tx_mksearch_indexer_ttcontent_Normal::IS_NOT_INDEXABLE
+					),
+				),
+				'default' => tx_mksearch_indexer_ttcontent_Normal::USE_INDEXER_CONFIGURATION
+			)
+		),
 	),
+	!tx_rnbase_util_TYPO3::isTYPO62OrHigher()
 );
 
-tx_rnbase_util_Extensions::addTCAcolumns('tt_content',$tempTtContent,1);
 tx_rnbase_util_Extensions::addToAllTCAtypes('tt_content', 'tx_mksearch_is_indexable');
