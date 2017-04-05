cfff507c60e5a89580c147dde5a87ce6d1cefe5c
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2017-04-05 10:36:52 +0200

    #25 use rnbase extension wrapper to register be module

--- mod1/ext_tables.php
+++ mod1/ext_tables.php
@@ -5,7 +5,7 @@ if (TYPO3_MODE == 'BE') {
 
 	if (tx_rnbase_util_TYPO3::isTYPO60OrHigher()) {
 		tx_rnbase::load('tx_mksearch_mod1_Module');
-		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+		tx_rnbase_util_Extensions::registerModule(
 			'mksearch',
 			'web',
 			'M1',
@@ -21,45 +21,45 @@ if (TYPO3_MODE == 'BE') {
 	} else {
 		tx_rnbase_util_Extensions::addModulePath(
 			'web_MksearchM1',
-			tx_rnbase_util_Extensions::extPath($_EXTKEY, 'mod1/')
+			tx_rnbase_util_Extensions::extPath('mksearch', 'mod1/')
 		);
 		tx_rnbase_util_Extensions::addModule(
 			'web',
 			'MksearchM1',
 			'bottom',
-			tx_rnbase_util_Extensions::extPath($_EXTKEY, 'mod1/')
+			tx_rnbase_util_Extensions::extPath('mksearch', 'mod1/')
 		);
 	}
 
 	tx_rnbase_util_Extensions::insertModuleFunction(
 		'web_MksearchM1',
 		'tx_mksearch_mod1_ConfigIndizes',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY, 'mod1/class.tx_mksearch_mod1_ConfigIndizes.php'),
+		tx_rnbase_util_Extensions::extPath('mksearch', 'mod1/class.tx_mksearch_mod1_ConfigIndizes.php'),
 		'LLL:EXT:mksearch/mod1/locallang.xml:func_config_indizes'
 	);
 	tx_rnbase_util_Extensions::insertModuleFunction(
 		'web_MksearchM1',
 		'tx_mksearch_mod1_Keywords',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY, 'mod1/class.tx_mksearch_mod1_Keywords.php'),
+		tx_rnbase_util_Extensions::extPath('mksearch', 'mod1/class.tx_mksearch_mod1_Keywords.php'),
 		'LLL:EXT:mksearch/mod1/locallang.xml:func_keywords'
 	);
 	tx_rnbase_util_Extensions::insertModuleFunction(
 		'web_MksearchM1',
 		'tx_mksearch_mod1_IndizeIndizes',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY, 'mod1/class.tx_mksearch_mod1_IndizeIndizes.php'),
+		tx_rnbase_util_Extensions::extPath('mksearch', 'mod1/class.tx_mksearch_mod1_IndizeIndizes.php'),
 		'LLL:EXT:mksearch/mod1/locallang.xml:func_indize_indizes'
 	);
 	tx_rnbase_util_Extensions::insertModuleFunction(
 		'web_MksearchM1',
 		'tx_mksearch_mod1_SolrAdmin',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY, 'mod1/class.tx_mksearch_mod1_SolrAdmin.php'),
+		tx_rnbase_util_Extensions::extPath('mksearch', 'mod1/class.tx_mksearch_mod1_SolrAdmin.php'),
 		'LLL:EXT:mksearch/mod1/locallang.xml:func_solradmin'
 	);
 
 // 	tx_rnbase_util_Extensions::insertModuleFunction(
 // 		'web_MksearchM1',
 // 		'tx_mksearch_mod1_ConfigIndizesDbList',
-// 		tx_rnbase_util_Extensions::extPath($_EXTKEY, 'mod1/class.tx_mksearch_mod1_ConfigIndizesDbList.php'),
+// 		tx_rnbase_util_Extensions::extPath('mksearch', 'mod1/class.tx_mksearch_mod1_ConfigIndizesDbList.php'),
 // 		'LLL:EXT:mksearch/mod1/locallang.xml:func_config_indizesdblist'
 // 	);
 
