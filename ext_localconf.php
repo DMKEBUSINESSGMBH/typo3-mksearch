3ac7d64894f59eec02b4bf13f57b04b4a7c89860
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2019-05-20 12:37:55 +0200

    #46 hook configuration without filename

--- ext_localconf.php
+++ ext_localconf.php
@@ -24,28 +24,28 @@ require_once tx_rnbase_util_Extensions::extPath('mksearch').'indexer/ext_localco
 // rnbase insert and update hooks (requires rn_base 0.14.6)
 if (isset($_EXTCONF['enableRnBaseUtilDbHook']) && (int) $_EXTCONF['enableRnBaseUtilDbHook'] > 0) {
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['rn_base']['util_db_do_insert_post'][] =
-        'EXT:mksearch/hooks/class.tx_mksearch_hooks_IndexerAutoUpdate.php:tx_mksearch_hooks_IndexerAutoUpdate->rnBaseDoInsertPost';
+        'tx_mksearch_hooks_IndexerAutoUpdate->rnBaseDoInsertPost';
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['rn_base']['util_db_do_update_post'][] =
-        'EXT:mksearch/hooks/class.tx_mksearch_hooks_IndexerAutoUpdate.php:tx_mksearch_hooks_IndexerAutoUpdate->rnBaseDoUpdatePost';
+        'tx_mksearch_hooks_IndexerAutoUpdate->rnBaseDoUpdatePost';
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['rn_base']['util_db_do_delete_pre'][] =
-        'EXT:mksearch/hooks/class.tx_mksearch_hooks_IndexerAutoUpdate.php:tx_mksearch_hooks_IndexerAutoUpdate->rnBaseDoDeletePre';
+        'tx_mksearch_hooks_IndexerAutoUpdate->rnBaseDoDeletePre';
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['rn_base']['util_db_do_select_pre'][] =
-    'EXT:mksearch/hooks/class.tx_mksearch_hooks_DatabaseConnection.php:tx_mksearch_hooks_DatabaseConnection->doSelectPre';
+    'tx_mksearch_hooks_DatabaseConnection->doSelectPre';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['rn_base']['util_db_do_select_post'][] =
-    'EXT:mksearch/hooks/class.tx_mksearch_hooks_DatabaseConnection.php:tx_mksearch_hooks_DatabaseConnection->doSelectPost';
+    'tx_mksearch_hooks_DatabaseConnection->doSelectPost';
 
 // Hook for manipulating a single search term used with Zend_Lucene
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['mksearch']['engine_ZendLucene_buildQuery_manipulateSingleTerm'][] =
-    'EXT:'.$_EXTKEY.'/hooks/class.tx_mksearch_hooks_EngineZendLucene.php:tx_mksearch_hooks_EngineZendLucene->manipulateSingleTerm';
+    'tx_mksearch_hooks_EngineZendLucene->manipulateSingleTerm';
 
 // Hooks for auto-updating search indices
 // TODO: Die Hooks per EM-Config zuschaltbar machen
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
-    'EXT:'.$_EXTKEY.'/hooks/class.tx_mksearch_hooks_IndexerAutoUpdate.php:tx_mksearch_hooks_IndexerAutoUpdate';
+    'tx_mksearch_hooks_IndexerAutoUpdate';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] =
-    'EXT:'.$_EXTKEY.'/hooks/class.tx_mksearch_hooks_IndexerAutoUpdate.php:tx_mksearch_hooks_IndexerAutoUpdate';
+    'tx_mksearch_hooks_IndexerAutoUpdate';
 // Include PageTSConfig for backend module
 tx_rnbase_util_Extensions::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mksearch/mod1/pageTSconfig.txt">');
 
@@ -62,13 +62,11 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_mksearch_sch
 );
 
 if (tx_rnbase_util_Extensions::isLoaded('mksanitizedparameters')) {
-    require_once tx_rnbase_util_Extensions::extPath($_EXTKEY).'ext_mksanitizedparameter_rules.php';
+    require_once tx_rnbase_util_Extensions::extPath($_EXTKEY, 'ext_mksanitizedparameter_rules.php');
 }
 
-if (tx_rnbase_util_TYPO3::isTYPO60OrHigher()) {
-    require_once tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/SignalSlotDispatcher.php';
-    require_once tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/XClasses.php';
-}
+require_once tx_rnbase_util_Extensions::extPath($_EXTKEY, 'Configuration/SignalSlotDispatcher.php');
+require_once tx_rnbase_util_Extensions::extPath($_EXTKEY,'Configuration/XClasses.php');
 
 tx_rnbase::load('tx_rnbase_util_Misc');
 tx_rnbase::load('tx_rnbase_util_Strings');
