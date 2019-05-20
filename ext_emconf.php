76d9749c26298a41746adde014a95f59db2c42a3
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2019-05-20 11:42:05 +0200

    #46 initial wip 9.5 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,8 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'rn_base' => '1.6.3-',
-            'typo3' => '4.5.0-8.7.99',
-            'scheduler' => '1.0.0-8.7.99'
+            'typo3' => '4.5.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
