c3dca3e4f60bca87f11986d8d4283ae31d5227ab
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2020-10-26 14:32:48 +0100

    cleanup added mksanitizedparameter rule for fq parameter

--- ext_mksanitizedparameter_rules.php
+++ ext_mksanitizedparameter_rules.php
@@ -2,15 +2,15 @@
 
 defined('TYPO3_MODE') || die('Access denied.');
 
-// $rulesForFrontend = array(
-//     'mksearch'    => array(
-//        // wird in Solr verwendet und wird vor der eigentlichen Suche bereinigt.
-//        // Also hier nix machen.
-//        'term' => FILTER_UNSAFE_RAW,
-//     ),
-// );
-
-$rulesForFrontend =
-    unserialize('a:1:{s:8:"mksearch";a:1:{s:4:"term";i:516;}}');
+ $rulesForFrontend = [
+     'mksearch' => [
+        // wird in Solr verwendet und wird vor der eigentlichen Suche bereinigt.
+        // Also hier nix machen.
+        'term' => FILTER_UNSAFE_RAW,
+         'fq' => [
+             tx_mksanitizedparameters_Rules::DEFAULT_RULES_KEY => FILTER_UNSAFE_RAW,
+         ],
+     ],
+ ];
 
 tx_mksanitizedparameters_Rules::addRulesForFrontend($rulesForFrontend);
