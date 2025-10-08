fb5b8d5a8bf61e69ab22b71aa14738d26cf2d88b
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2025-10-08 14:21:27 +0200

    Cleanup exclude all pagebrowser pointers

--- ext_localconf.php
+++ ext_localconf.php
@@ -63,7 +63,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('mksanitizedpar
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('mksearch', 'Configuration/XClasses.php');
 
 \Sys25\RnBase\Utility\CHashUtility::addExcludedParametersForCacheHash([
-    'mksearch[pb-search-pointer]',
+    '^mksearch[pb-',
     'mksearch[submit]',
     'mksearch[term]',
     'mksearch[sort]',
