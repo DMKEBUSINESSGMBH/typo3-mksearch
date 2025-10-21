db29c63628faf353275a3087d8cc6c56c4e64e8e
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2025-10-21 10:18:28 +0200

    Bugfix fix invalid key

--- ext_tables.sql
+++ ext_tables.sql
@@ -136,8 +136,7 @@ CREATE TABLE tx_mksearch_keywords (
     link tinytext DEFAULT '' NOT NULL,
 
     PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY keyword (keyword)
+    KEY parent (pid)
 );
 
 #
